e2a06dd10f444b86e2badb3d9076be63daa78597
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-06-21 09:48:49 -0300

    Chore: configure GraphCMS and Apollo Client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { gql, useQuery } from '@apollo/client';
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 export function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY);
+  console.log(data);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-zinc-500">Event Page</h1>
+      <ul>
+        {data?.lessons.map((lesson: Lesson) => {
+          return <li key={lesson.id}>{lesson.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
