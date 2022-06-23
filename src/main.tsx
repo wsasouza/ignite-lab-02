f630c2adc0f58ff51cb8faee532e7db1d2a50c7f
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-06-23 14:25:50 -0300

    Feat: Finish Video Component and Create Routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ApolloProvider } from '@apollo/client';
-import { client } from './lib/apollo';
 
 import { App } from './App';
 
@@ -9,8 +7,6 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 );
